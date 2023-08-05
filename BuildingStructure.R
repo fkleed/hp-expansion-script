bd935f28e62950403b73b3a212581beefecff706
Ferenc Kleedörfer <ferenc.kleedoerfer@outlook.de>
2023-08-05 14:20:32 +0200

    first commit

---
+++ BuildingStructure.R
@@ -0,0 +1,8 @@
+# Load required packages
+library(tidyverse)
+
+# Load data
+nuts3regioninfo <- read_csv2("data/nuts3regioninfo.csv")
+heatinginfo <- read_csv("data/heatinginfo.csv")
+
+# Transform data
