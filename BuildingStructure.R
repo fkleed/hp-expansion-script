c6e35abf7bae817a37ec8fd492a390af6f0e3893
Ferenc Kleedörfer <ferenc.kleedoerfer@outlook.de>
2023-08-06 19:13:58 +0200

    added count for new dormitories and other buildings

--- BuildingStructure.R
+++ BuildingStructure.R
@@ -4,7 +4,7 @@ library(readxl)
 library("dplyr")
 
 
-# Load data
+# Load heating and region info
 nuts3regioninfo <- read_excel("data/nuts3regioninfo.xlsx")
 
 heatinginfo_without_dormitories <-
@@ -13,10 +13,6 @@ heatinginfo_without_dormitories <-
 heatinginfo_only_dormitories <-
   read_csv("data/heatinginfo_only_dormitories.csv")
 
-
-# Transform data
-
-# Reduce categories for the building size
 nuts3regioninfo <-
   nuts3regioninfo %>% mutate_if(is.character, as.factor)
 
@@ -26,6 +22,8 @@ heatinginfo_without_dormitories <-
 heatinginfo_only_dormitories <-
   heatinginfo_only_dormitories %>% mutate_if(is.character, as.factor)
 
+
+# Reduce categories for the building size
 heatinginfo_without_dormitories <-
   heatinginfo_without_dormitories %>%
   mutate(
@@ -44,8 +42,8 @@ heatinginfo_without_dormitories <-
     )
   ) %>% group_by(BuildingTypeSize,
                  HeatingType,
-                 `NUTS-3-Code`,
-                 YearOfConstruction) %>% summarise(`Sum of BuildingCount` = sum(`Sum of BuildingCount`),
+                 NUTS3Code,
+                 YearOfConstruction) %>% summarise(BuildingCount = sum(BuildingCount),
                                                    .groups = 'drop')
 
 heatinginfo_only_dormitories <-
@@ -67,10 +65,62 @@ heatinginfo_only_dormitories <-
     )
   ) %>% group_by(BuildingTypeSize,
                  HeatingType,
-                 `NUTS-3-Code`,
-                 YearOfConstruction) %>% summarise(`Sum of BuildingCount` = sum(`Sum of BuildingCount`),
+                 NUTS3Code,
+                 YearOfConstruction) %>% summarise(BuildingCount = sum(BuildingCount),
                                                    .groups = 'drop')
 
+
+# Predict distribution of Building Stock for 2022
+# Assumption: Buildings without dormitories are distributed on a federal-state basis like the distribution from 2001 onwards
+
+building_stock_2022 <- read_csv("data/building_stock_2022.csv") %>%
+  mutate_if(is.character, as.factor) %>%
+  left_join(distinct(select(
+    nuts3regioninfo, "NUTS1Name", "NUTS1Code"
+  )),
+  by = c("FederalState" = "NUTS1Name"))
+
+new_buildings_without_dormitories_by_state <-
+  heatinginfo_without_dormitories %>%
+  left_join(select(nuts3regioninfo, c("NUTS1Code", "NUTS3Code")), by = "NUTS3Code") %>%
+  group_by(NUTS1Code) %>%
+  summarise(BuildingCount = sum(BuildingCount),
+            .groups = 'drop') %>%
+  left_join(select(building_stock_2022, c("BuildingsWithoutDormitoriesCount", "NUTS1Code")),
+            by = "NUTS1Code") %>%
+  mutate(NewBuildingsWithoutDormitoriesCount = BuildingsWithoutDormitoriesCount - BuildingCount) %>%
+  select(c("NUTS1Code", "NewBuildingsWithoutDormitoriesCount"))
+
+new_dormitories_by_state <-
+  heatinginfo_only_dormitories %>%
+  left_join(select(nuts3regioninfo, c("NUTS1Code", "NUTS3Code")), by = "NUTS3Code") %>%
+  group_by(NUTS1Code) %>%
+  summarise(BuildingCount = sum(BuildingCount),
+            .groups = 'drop') %>%
+  left_join(select(building_stock_2022, c("DormitoriesCount", "NUTS1Code")),
+            by = "NUTS1Code") %>%
+  mutate(NewDormitoriesCount = DormitoriesCount - BuildingCount) %>%
+  select(c("NUTS1Code", "NewDormitoriesCount"))
+
+
+
+summary(building_stock_2022)
+
+
+
+
+levels(heatinginfo_without_dormitories$YearOfConstruction)
+
+test <- heatinginfo_without_dormitories %>%
+  filter(YearOfConstruction %in% c("2001 - 2004", "2005 - 2008", "2009 und später")) %>%
+  group_by(BuildingTypeSize, HeatingType) %>%
+  summarise(BuildingCount = sum(BuildingCount),
+            .groups = 'drop') %>%
+  mutate(Percentage = round(BuildingCount / sum(BuildingCount) * 100, 2))
+
+
+# Write output to csv
+write_csv2(heatinginfo, "data/output.csv")
 
 
 # Optional: check if numbers are consistent with building stock
@@ -85,16 +135,11 @@ heatinginfo_only_dormitories %>%
   group_by(`NUTS1-Name`) %>%
   summarise(sum = sum(`Sum of BuildingCount`))
 
-# Optional: Remove later the building types without category
+# Optional: Remove the building types without category
 heatinginfo_without_dormitories <-
-  subset(heatinginfo_without_dormitories, BuildingTypeSize != "Anderer Gebäudetyp")
+  subset(heatinginfo_without_dormitories,
+         BuildingTypeSize != "Anderer Gebäudetyp")
 
 heatinginfo_only_dormitories <-
-  subset(heatinginfo_only_dormitories, BuildingTypeSize != "Anderer Gebäudetyp")
-
-# Predict distribution of Building Stock for 2022
-
-
-
-# Write output to csv
-write_csv2(heatinginfo, "data/output.csv")
+  subset(heatinginfo_only_dormitories,
+         BuildingTypeSize != "Anderer Gebäudetyp")
