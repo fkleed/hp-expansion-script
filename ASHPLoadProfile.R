7342216132ecd65750c5b5f2ac67b16e104b263b
Ferenc Kleedörfer <ferenc.kleedoerfer@outlook.de>
2023-08-12 19:57:40 +0200

    Added mixed temperature

--- ASHPLoadProfile.R
+++ ASHPLoadProfile.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(readxl)
 library("dplyr")
+library(zoo)
 
 # Read the heat demand data
 eh_combined_heat_demand_avg <-
@@ -26,29 +27,57 @@ mh_combined_heat_demand_hot <-
 weather_data_avg <-
   read_csv2("data/loadprofile/weatherdata/averageyear.csv") %>%
   mutate(
-    RoundedMeanTemperature = as.numeric(RoundedMeanTemperature),
+    TemperatureKelvin = as.numeric(RoundedMeanTemperature) + 273.15,
     Time = paste(paste(substr(Date, 3, 4), substr(Date, 1, 2), sep = "-"),
                  paste(substr(Date, 5, 6), "00", sep = ":"))
   ) %>%
-  select(-c(Date, MeanTemperature))
+  select(-c(Date, MeanTemperature, RoundedMeanTemperature))
 
 
 weather_data_cold <-
   read_csv2("data/loadprofile/weatherdata/year2010.csv") %>%
   mutate(
-    RoundedMeanTemperature = as.numeric(RoundedMeanTemperature),
+    TemperatureKelvin = as.numeric(RoundedMeanTemperature) + 273.15,
     Time = paste(paste(substr(Date, 3, 4), substr(Date, 1, 2), sep = "-"),
                  paste(substr(Date, 5, 6), "00", sep = ":"))
   ) %>%
-  select(-c(Date, MeanTemperature))
+  select(-c(Date, MeanTemperature, RoundedMeanTemperature))
 
 weather_data_hot <-
   read_csv2("data/loadprofile/weatherdata/year2022.csv") %>%
   mutate(
-    RoundedMeanTemperature = as.numeric(RoundedMeanTemperature),
+    TemperatureKelvin = as.numeric(RoundedMeanTemperature) + 273.15,
     Time = paste(paste(substr(Date, 3, 4), substr(Date, 1, 2), sep = "-"),
                  paste(substr(Date, 5, 6), "00", sep = ":"))
   ) %>%
-  select(-c(Date, MeanTemperature))
+  select(-c(Date, MeanTemperature, RoundedMeanTemperature))
 
 
+# Calculate the hourly mixed temperature of the weather data
+weather_data_avg <-
+  tibble::rowid_to_column(weather_data_avg, "RowNumber")
+
+weather_data_avg  <-
+  weather_data_avg %>% mutate(T72WeightedAverage = round(rollmeanr(TemperatureKelvin,
+                                                                   72,
+                                                                   fill = NA), 2))
+
+weather_data_avg_until_71 <-
+  weather_data_avg %>% filter(RowNumber < 72) %>%
+  mutate(CumSumTemperatureKelvin = cumsum(TemperatureKelvin)) %>%
+  mutate(T72WeightedAverage = round(CumSumTemperatureKelvin / RowNumber, 2)) %>%
+  select(-c(CumSumTemperatureKelvin))
+
+weather_data_avg <- weather_data_avg %>%
+  filter(RowNumber >= 72)
+
+weather_data_avg <-
+  rbind(weather_data_avg_until_71, weather_data_avg)
+
+weather_data_avg <- weather_data_avg %>%
+  mutate(TMix = ifelse(
+    substr(Time, 7, 8) %in% c("10", "11", "12", "13", "14", "15", "16"),
+    TemperatureKelvin,
+    T72WeightedAverage
+  )) %>%
+  select(-c(RowNumber, T72WeightedAverage))
