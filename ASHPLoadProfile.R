9389302501dda19cfb2f1239cc155cad68ccd496
Ferenc Kleedörfer <ferenc.kleedoerfer@outlook.de>
2023-08-14 21:10:10 +0200

    Fixed TMix bug and added COP for multi family homes

--- ASHPLoadProfile.R
+++ ASHPLoadProfile.R
@@ -440,51 +440,6 @@ eh_combined_heat_demand_cold <- eh_combined_heat_demand_cold %>%
 eh_combined_heat_demand_hot <- eh_combined_heat_demand_hot %>%
   left_join(weather_data_hot, by = "Time")
 
-
-eh_combined_heat_demand_avg <- eh_combined_heat_demand_avg %>%
-  mutate(
-    SpaceHeat_beginn_1918_avg = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_beginn_1918_avg),
-    SpaceHeat_1919_1948_avg = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1919_1948_avg),
-    SpaceHeat_1949_1978_avg = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1949_1978_avg),
-    SpaceHeat_1979_1986_avg = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1979_1986_avg),
-    SpaceHeat_1987_1990_avg = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1987_1990_avg),
-    SpaceHeat_1991_1995_avg = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1991_1995_avg),
-    SpaceHeat_1996_2000_avg = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1996_2000_avg),
-    SpaceHeat_2001_2011_avg = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_2001_2011_avg),
-    SpaceHeat_2012_2022_avg = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_2012_2022_avg),
-    SpaceHeat_2023_2030_avg = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_2023_2030_avg)
-  )
-
-
-eh_combined_heat_demand_cold <- eh_combined_heat_demand_cold %>%
-  mutate(
-    SpaceHeat_beginn_1918_cold = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_beginn_1918_cold),
-    SpaceHeat_1919_1948_cold = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1919_1948_cold),
-    SpaceHeat_1949_1978_cold = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1949_1978_cold),
-    SpaceHeat_1979_1986_cold = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1979_1986_cold),
-    SpaceHeat_1987_1990_cold = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1987_1990_cold),
-    SpaceHeat_1991_1995_cold = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1991_1995_cold),
-    SpaceHeat_1996_2000_cold = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1996_2000_cold),
-    SpaceHeat_2001_2011_cold = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_2001_2011_cold),
-    SpaceHeat_2012_2022_cold = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_2012_2022_cold),
-    SpaceHeat_2023_2030_cold = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_2023_2030_cold)
-  )
-
-eh_combined_heat_demand_hot <- eh_combined_heat_demand_hot %>%
-  mutate(
-    SpaceHeat_beginn_1918_hot = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_beginn_1918_hot),
-    SpaceHeat_1919_1948_hot = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1919_1948_hot),
-    SpaceHeat_1949_1978_hot = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1949_1978_hot),
-    SpaceHeat_1979_1986_hot = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1979_1986_hot),
-    SpaceHeat_1987_1990_hot = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1987_1990_hot),
-    SpaceHeat_1991_1995_hot = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1991_1995_hot),
-    SpaceHeat_1996_2000_hot = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1996_2000_hot),
-    SpaceHeat_2001_2011_hot = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_2001_2011_hot),
-    SpaceHeat_2012_2022_hot = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_2012_2022_hot),
-    SpaceHeat_2023_2030_hot = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_2023_2030_hot)
-  )
-
-
 mh_combined_heat_demand_avg <- mh_combined_heat_demand_avg %>%
   left_join(weather_data_avg, by = "Time")
 
@@ -494,48 +449,90 @@ mh_combined_heat_demand_cold <- mh_combined_heat_demand_cold %>%
 mh_combined_heat_demand_hot <- mh_combined_heat_demand_hot %>%
   left_join(weather_data_hot, by = "Time")
 
+eh_combined_heat_demand_avg <- eh_combined_heat_demand_avg %>%
+  mutate(
+    SpaceHeat_beginn_1918_avg = ifelse(TMix >= 293.15, 0,  SpaceHeat_beginn_1918_avg),
+    SpaceHeat_1919_1948_avg = ifelse(TMix >= 293.15, 0,  SpaceHeat_1919_1948_avg),
+    SpaceHeat_1949_1978_avg = ifelse(TMix >= 293.15, 0,  SpaceHeat_1949_1978_avg),
+    SpaceHeat_1979_1986_avg = ifelse(TMix >= 293.15, 0,  SpaceHeat_1979_1986_avg),
+    SpaceHeat_1987_1990_avg = ifelse(TMix >= 293.15, 0,  SpaceHeat_1987_1990_avg),
+    SpaceHeat_1991_1995_avg = ifelse(TMix >= 293.15, 0,  SpaceHeat_1991_1995_avg),
+    SpaceHeat_1996_2000_avg = ifelse(TMix >= 293.15, 0,  SpaceHeat_1996_2000_avg),
+    SpaceHeat_2001_2011_avg = ifelse(TMix >= 293.15, 0,  SpaceHeat_2001_2011_avg),
+    SpaceHeat_2012_2022_avg = ifelse(TMix >= 293.15, 0,  SpaceHeat_2012_2022_avg),
+    SpaceHeat_2023_2030_avg = ifelse(TMix >= 293.15, 0,  SpaceHeat_2023_2030_avg)
+  )
+
+
+eh_combined_heat_demand_cold <- eh_combined_heat_demand_cold %>%
+  mutate(
+    SpaceHeat_beginn_1918_cold = ifelse(TMix >= 293.15, 0,  SpaceHeat_beginn_1918_cold),
+    SpaceHeat_1919_1948_cold = ifelse(TMix >= 293.15, 0,  SpaceHeat_1919_1948_cold),
+    SpaceHeat_1949_1978_cold = ifelse(TMix >= 293.15, 0,  SpaceHeat_1949_1978_cold),
+    SpaceHeat_1979_1986_cold = ifelse(TMix >= 293.15, 0,  SpaceHeat_1979_1986_cold),
+    SpaceHeat_1987_1990_cold = ifelse(TMix >= 293.15, 0,  SpaceHeat_1987_1990_cold),
+    SpaceHeat_1991_1995_cold = ifelse(TMix >= 293.15, 0,  SpaceHeat_1991_1995_cold),
+    SpaceHeat_1996_2000_cold = ifelse(TMix >= 293.15, 0,  SpaceHeat_1996_2000_cold),
+    SpaceHeat_2001_2011_cold = ifelse(TMix >= 293.15, 0,  SpaceHeat_2001_2011_cold),
+    SpaceHeat_2012_2022_cold = ifelse(TMix >= 293.15, 0,  SpaceHeat_2012_2022_cold),
+    SpaceHeat_2023_2030_cold = ifelse(TMix >= 293.15, 0,  SpaceHeat_2023_2030_cold)
+  )
+
+eh_combined_heat_demand_hot <- eh_combined_heat_demand_hot %>%
+  mutate(
+    SpaceHeat_beginn_1918_hot = ifelse(TMix >= 293.15, 0,  SpaceHeat_beginn_1918_hot),
+    SpaceHeat_1919_1948_hot = ifelse(TMix >= 293.15, 0,  SpaceHeat_1919_1948_hot),
+    SpaceHeat_1949_1978_hot = ifelse(TMix >= 293.15, 0,  SpaceHeat_1949_1978_hot),
+    SpaceHeat_1979_1986_hot = ifelse(TMix >= 293.15, 0,  SpaceHeat_1979_1986_hot),
+    SpaceHeat_1987_1990_hot = ifelse(TMix >= 293.15, 0,  SpaceHeat_1987_1990_hot),
+    SpaceHeat_1991_1995_hot = ifelse(TMix >= 293.15, 0,  SpaceHeat_1991_1995_hot),
+    SpaceHeat_1996_2000_hot = ifelse(TMix >= 293.15, 0,  SpaceHeat_1996_2000_hot),
+    SpaceHeat_2001_2011_hot = ifelse(TMix >= 293.15, 0,  SpaceHeat_2001_2011_hot),
+    SpaceHeat_2012_2022_hot = ifelse(TMix >= 293.15, 0,  SpaceHeat_2012_2022_hot),
+    SpaceHeat_2023_2030_hot = ifelse(TMix >= 293.15, 0,  SpaceHeat_2023_2030_hot)
+  )
 
 mh_combined_heat_demand_avg <- mh_combined_heat_demand_avg %>%
   mutate(
-    SpaceHeat_beginn_1918_avg = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_beginn_1918_avg),
-    SpaceHeat_1919_1948_avg = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1919_1948_avg),
-    SpaceHeat_1949_1978_avg = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1949_1978_avg),
-    SpaceHeat_1979_1986_avg = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1979_1986_avg),
-    SpaceHeat_1987_1990_avg = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1987_1990_avg),
-    SpaceHeat_1991_1995_avg = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1991_1995_avg),
-    SpaceHeat_1996_2000_avg = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1996_2000_avg),
-    SpaceHeat_2001_2011_avg = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_2001_2011_avg),
-    SpaceHeat_2012_2022_avg = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_2012_2022_avg),
-    SpaceHeat_2023_2030_avg = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_2023_2030_avg)
+    SpaceHeat_beginn_1918_avg = ifelse(TMix >= 293.15, 0,  SpaceHeat_beginn_1918_avg),
+    SpaceHeat_1919_1948_avg = ifelse(TMix >= 293.15, 0,  SpaceHeat_1919_1948_avg),
+    SpaceHeat_1949_1978_avg = ifelse(TMix >= 293.15, 0,  SpaceHeat_1949_1978_avg),
+    SpaceHeat_1979_1986_avg = ifelse(TMix >= 293.15, 0,  SpaceHeat_1979_1986_avg),
+    SpaceHeat_1987_1990_avg = ifelse(TMix >= 293.15, 0,  SpaceHeat_1987_1990_avg),
+    SpaceHeat_1991_1995_avg = ifelse(TMix >= 293.15, 0,  SpaceHeat_1991_1995_avg),
+    SpaceHeat_1996_2000_avg = ifelse(TMix >= 293.15, 0,  SpaceHeat_1996_2000_avg),
+    SpaceHeat_2001_2011_avg = ifelse(TMix >= 293.15, 0,  SpaceHeat_2001_2011_avg),
+    SpaceHeat_2012_2022_avg = ifelse(TMix >= 293.15, 0,  SpaceHeat_2012_2022_avg),
+    SpaceHeat_2023_2030_avg = ifelse(TMix >= 293.15, 0,  SpaceHeat_2023_2030_avg)
   )
 
 
 mh_combined_heat_demand_cold <- mh_combined_heat_demand_cold %>%
   mutate(
-    SpaceHeat_beginn_1918_cold = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_beginn_1918_cold),
-    SpaceHeat_1919_1948_cold = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1919_1948_cold),
-    SpaceHeat_1949_1978_cold = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1949_1978_cold),
-    SpaceHeat_1979_1986_cold = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1979_1986_cold),
-    SpaceHeat_1987_1990_cold = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1987_1990_cold),
-    SpaceHeat_1991_1995_cold = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1991_1995_cold),
-    SpaceHeat_1996_2000_cold = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1996_2000_cold),
-    SpaceHeat_2001_2011_cold = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_2001_2011_cold),
-    SpaceHeat_2012_2022_cold = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_2012_2022_cold),
-    SpaceHeat_2023_2030_cold = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_2023_2030_cold)
+    SpaceHeat_beginn_1918_cold = ifelse(TMix >= 293.15, 0,  SpaceHeat_beginn_1918_cold),
+    SpaceHeat_1919_1948_cold = ifelse(TMix >= 293.15, 0,  SpaceHeat_1919_1948_cold),
+    SpaceHeat_1949_1978_cold = ifelse(TMix >= 293.15, 0,  SpaceHeat_1949_1978_cold),
+    SpaceHeat_1979_1986_cold = ifelse(TMix >= 293.15, 0,  SpaceHeat_1979_1986_cold),
+    SpaceHeat_1987_1990_cold = ifelse(TMix >= 293.15, 0,  SpaceHeat_1987_1990_cold),
+    SpaceHeat_1991_1995_cold = ifelse(TMix >= 293.15, 0,  SpaceHeat_1991_1995_cold),
+    SpaceHeat_1996_2000_cold = ifelse(TMix >= 293.15, 0,  SpaceHeat_1996_2000_cold),
+    SpaceHeat_2001_2011_cold = ifelse(TMix >= 293.15, 0,  SpaceHeat_2001_2011_cold),
+    SpaceHeat_2012_2022_cold = ifelse(TMix >= 293.15, 0,  SpaceHeat_2012_2022_cold),
+    SpaceHeat_2023_2030_cold = ifelse(TMix >= 293.15, 0,  SpaceHeat_2023_2030_cold)
   )
 
 mh_combined_heat_demand_hot <- mh_combined_heat_demand_hot %>%
   mutate(
-    SpaceHeat_beginn_1918_hot = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_beginn_1918_hot),
-    SpaceHeat_1919_1948_hot = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1919_1948_hot),
-    SpaceHeat_1949_1978_hot = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1949_1978_hot),
-    SpaceHeat_1979_1986_hot = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1979_1986_hot),
-    SpaceHeat_1987_1990_hot = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1987_1990_hot),
-    SpaceHeat_1991_1995_hot = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1991_1995_hot),
-    SpaceHeat_1996_2000_hot = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_1996_2000_hot),
-    SpaceHeat_2001_2011_hot = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_2001_2011_hot),
-    SpaceHeat_2012_2022_hot = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_2012_2022_hot),
-    SpaceHeat_2023_2030_hot = ifelse(TemperatureKelvin >= 293.15, 0,  SpaceHeat_2023_2030_hot)
+    SpaceHeat_beginn_1918_hot = ifelse(TMix >= 293.15, 0,  SpaceHeat_beginn_1918_hot),
+    SpaceHeat_1919_1948_hot = ifelse(TMix >= 293.15, 0,  SpaceHeat_1919_1948_hot),
+    SpaceHeat_1949_1978_hot = ifelse(TMix >= 293.15, 0,  SpaceHeat_1949_1978_hot),
+    SpaceHeat_1979_1986_hot = ifelse(TMix >= 293.15, 0,  SpaceHeat_1979_1986_hot),
+    SpaceHeat_1987_1990_hot = ifelse(TMix >= 293.15, 0,  SpaceHeat_1987_1990_hot),
+    SpaceHeat_1991_1995_hot = ifelse(TMix >= 293.15, 0,  SpaceHeat_1991_1995_hot),
+    SpaceHeat_1996_2000_hot = ifelse(TMix >= 293.15, 0,  SpaceHeat_1996_2000_hot),
+    SpaceHeat_2001_2011_hot = ifelse(TMix >= 293.15, 0,  SpaceHeat_2001_2011_hot),
+    SpaceHeat_2012_2022_hot = ifelse(TMix >= 293.15, 0,  SpaceHeat_2012_2022_hot),
+    SpaceHeat_2023_2030_hot = ifelse(TMix >= 293.15, 0,  SpaceHeat_2023_2030_hot)
   )
 
 
@@ -1615,4 +1612,164 @@ eh_combined_heat_demand_hot <- eh_combined_heat_demand_hot %>%
     )
   )
 
+mh_combined_heat_demand_avg <- mh_combined_heat_demand_avg %>%
+  mutate(
+    COPHPTheo_beginn_1918_avg = (
+      ShareHP_beginn_1918_avg * (FlowTemp_beginn_1918_avg  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_beginn_1918_avg * (FlowTemp_beginn_1918_avg - TemperatureKelvin)
+    ),
+    COPHPTheo_1919_1948_avg = (
+      ShareHP_1919_1948_avg * (FlowTemp_1919_1948_avg  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_1919_1948_avg * (FlowTemp_1919_1948_avg - TemperatureKelvin)
+    ),
+    COPHPTheo_1949_1978_avg = (
+      ShareHP_1949_1978_avg * (FlowTemp_1949_1978_avg  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_1949_1978_avg * (FlowTemp_1949_1978_avg - TemperatureKelvin)
+    ),
+    COPHPTheo_1979_1986_avg = (
+      ShareHP_1979_1986_avg * (FlowTemp_1979_1986_avg  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_1979_1986_avg * (FlowTemp_1979_1986_avg - TemperatureKelvin)
+    ),
+    COPHPTheo_1987_1990_avg = (
+      ShareHP_1987_1990_avg * (FlowTemp_1987_1990_avg  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_1987_1990_avg * (FlowTemp_1987_1990_avg - TemperatureKelvin)
+    ),
+    COPHPTheo_1991_1995_avg = (
+      ShareHP_1991_1995_avg * (FlowTemp_1991_1995_avg  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_1991_1995_avg * (FlowTemp_1991_1995_avg - TemperatureKelvin)
+    ),
+    COPHPTheo_1996_2000_avg = (
+      ShareHP_1996_2000_avg * (FlowTemp_1996_2000_avg  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_1996_2000_avg * (FlowTemp_1996_2000_avg - TemperatureKelvin)
+    ),
+    COPHPTheo_2001_2011_avg = (
+      ShareHP_2001_2011_avg * (FlowTemp_2001_2011_avg  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_2001_2011_avg * (FlowTemp_2001_2011_avg - TemperatureKelvin)
+    ),
+    COPHPTheo_2012_2022_avg = (
+      ShareHP_2012_2022_avg * (FlowTemp_2012_2022_avg  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_2012_2022_avg * (FlowTemp_2012_2022_avg - TemperatureKelvin)
+    ),
+    COPHPTheo_2023_2030_avg = (
+      ShareHP_2023_2030_avg * (FlowTemp_2023_2030_avg  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_2023_2030_avg * (FlowTemp_2023_2030_avg - TemperatureKelvin)
+    )
+  )
 
+mh_combined_heat_demand_cold <- mh_combined_heat_demand_cold %>%
+  mutate(
+    COPHPTheo_beginn_1918_cold = (
+      ShareHP_beginn_1918_cold * (FlowTemp_beginn_1918_cold  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_beginn_1918_cold * (FlowTemp_beginn_1918_cold - TemperatureKelvin)
+    ),
+    COPHPTheo_1919_1948_cold = (
+      ShareHP_1919_1948_cold * (FlowTemp_1919_1948_cold  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_1919_1948_cold * (FlowTemp_1919_1948_cold - TemperatureKelvin)
+    ),
+    COPHPTheo_1949_1978_cold = (
+      ShareHP_1949_1978_cold * (FlowTemp_1949_1978_cold  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_1949_1978_cold * (FlowTemp_1949_1978_cold - TemperatureKelvin)
+    ),
+    COPHPTheo_1979_1986_cold = (
+      ShareHP_1979_1986_cold * (FlowTemp_1979_1986_cold  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_1979_1986_cold * (FlowTemp_1979_1986_cold - TemperatureKelvin)
+    ),
+    COPHPTheo_1987_1990_cold = (
+      ShareHP_1987_1990_cold * (FlowTemp_1987_1990_cold  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_1987_1990_cold * (FlowTemp_1987_1990_cold - TemperatureKelvin)
+    ),
+    COPHPTheo_1991_1995_cold = (
+      ShareHP_1991_1995_cold * (FlowTemp_1991_1995_cold  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_1991_1995_cold * (FlowTemp_1991_1995_cold - TemperatureKelvin)
+    ),
+    COPHPTheo_1996_2000_cold = (
+      ShareHP_1996_2000_cold * (FlowTemp_1996_2000_cold  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_1996_2000_cold * (FlowTemp_1996_2000_cold - TemperatureKelvin)
+    ),
+    COPHPTheo_2001_2011_cold = (
+      ShareHP_2001_2011_cold * (FlowTemp_2001_2011_cold  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_2001_2011_cold * (FlowTemp_2001_2011_cold - TemperatureKelvin)
+    ),
+    COPHPTheo_2012_2022_cold = (
+      ShareHP_2012_2022_cold * (FlowTemp_2012_2022_cold  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_2012_2022_cold * (FlowTemp_2012_2022_cold - TemperatureKelvin)
+    ),
+    COPHPTheo_2023_2030_cold = (
+      ShareHP_2023_2030_cold * (FlowTemp_2023_2030_cold  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_2023_2030_cold * (FlowTemp_2023_2030_cold - TemperatureKelvin)
+    )
+  )
+
+mh_combined_heat_demand_hot <- mh_combined_heat_demand_hot %>%
+  mutate(
+    COPHPTheo_beginn_1918_hot = (
+      ShareHP_beginn_1918_hot * (FlowTemp_beginn_1918_hot  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_beginn_1918_hot * (FlowTemp_beginn_1918_hot - TemperatureKelvin)
+    ),
+    COPHPTheo_1919_1948_hot = (
+      ShareHP_1919_1948_hot * (FlowTemp_1919_1948_hot  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_1919_1948_hot * (FlowTemp_1919_1948_hot - TemperatureKelvin)
+    ),
+    COPHPTheo_1949_1978_hot = (
+      ShareHP_1949_1978_hot * (FlowTemp_1949_1978_hot  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_1949_1978_hot * (FlowTemp_1949_1978_hot - TemperatureKelvin)
+    ),
+    COPHPTheo_1979_1986_hot = (
+      ShareHP_1979_1986_hot * (FlowTemp_1979_1986_hot  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_1979_1986_hot * (FlowTemp_1979_1986_hot - TemperatureKelvin)
+    ),
+    COPHPTheo_1987_1990_hot = (
+      ShareHP_1987_1990_hot * (FlowTemp_1987_1990_hot  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_1987_1990_hot * (FlowTemp_1987_1990_hot - TemperatureKelvin)
+    ),
+    COPHPTheo_1991_1995_hot = (
+      ShareHP_1991_1995_hot * (FlowTemp_1991_1995_hot  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_1991_1995_hot * (FlowTemp_1991_1995_hot - TemperatureKelvin)
+    ),
+    COPHPTheo_1996_2000_hot = (
+      ShareHP_1996_2000_hot * (FlowTemp_1996_2000_hot  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_1996_2000_hot * (FlowTemp_1996_2000_hot - TemperatureKelvin)
+    ),
+    COPHPTheo_2001_2011_hot = (
+      ShareHP_2001_2011_hot * (FlowTemp_2001_2011_hot  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_2001_2011_hot * (FlowTemp_2001_2011_hot - TemperatureKelvin)
+    ),
+    COPHPTheo_2012_2022_hot = (
+      ShareHP_2012_2022_hot * (FlowTemp_2012_2022_hot  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_2012_2022_hot * (FlowTemp_2012_2022_hot - TemperatureKelvin)
+    ),
+    COPHPTheo_2023_2030_hot = (
+      ShareHP_2023_2030_hot * (FlowTemp_2023_2030_hot  - TemperatureKelvin) + TemperatureKelvin
+    ) / (
+      ShareHP_2023_2030_hot * (FlowTemp_2023_2030_hot - TemperatureKelvin)
+    )
+  )
