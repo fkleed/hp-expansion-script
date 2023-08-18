e5f40b15def05b7e1cb9f214cf07bac5881ed049
Ferenc Kleedörfer <ferenc.kleedoerfer@outlook.de>
2023-08-18 12:47:43 +0200

    Reworked BuildingStructure

---
+++ HeatPumpPotential.R
@@ -0,0 +1,12 @@
+# Load required packages
+library(tidyverse)
+library(readxl)
+library("dplyr")
+
+
+# Read the building stock data
+building_stock_2030 <- read_csv2()
+
+# Read the nuts region info data
+
+# Read the heat pump potential data
