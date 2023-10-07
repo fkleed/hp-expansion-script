943be24f81b97cf4ecbf2557183649c5b9f2d50c
Ferenc Kleedörfer <ferenc.kleedoerfer@outlook.de>
2023-10-07 17:40:10 +0200

    Added electricity demand germany and updated corresponding plots

---
+++ findings/ElectricityDemandGermany.R
@@ -0,0 +1,237 @@
+# Load required packages
+library(tidyverse)
+library(readxl)
+library("dplyr")
+
+# Read the regions electricity demand data
+regions_electricity_demand_reference <-
+  read_csv("data/regionselectricitydemand/regions_electricity_demand_reference.csv")
+
+regions_electricity_demand_cold <-
+  read_csv("data/regionselectricitydemand/regions_electricity_demand_cold.csv")
+
+regions_electricity_demand_hot <-
+  read_csv("data/regionselectricitydemand/regions_electricity_demand_hot.csv")
+
+
+regions_electricity_demand_space_heat_only_reference <-
+  read_csv(
+    "data/regionselectricitydemand/regions_electricity_demand_space_heat_only_reference.csv"
+  )
+
+regions_electricity_demand_space_heat_only_cold <-
+  read_csv(
+    "data/regionselectricitydemand/regions_electricity_demand_space_heat_only_cold.csv"
+  )
+
+regions_electricity_demand_space_heat_only_hot <-
+  read_csv(
+    "data/regionselectricitydemand/regions_electricity_demand_space_heat_only_hot.csv"
+  )
+
+# Aggregate region electricity demand data to level of whole Germany
+
+# Space heat and hot water
+electricity_demand_reference_germany_sh_and_hw <-
+  regions_electricity_demand_reference %>%
+  select(-c("nuts3_code")) %>%
+  group_by(time) %>%
+  summarise("Electricity demand" = sum(hourly_electricity_demand),
+            .groups = "drop")
+
+electricity_demand_reference_germany_sh_and_hw <-
+  electricity_demand_reference_germany_sh_and_hw %>%
+  mutate(DATEISO = ISOdate(
+    2030,
+    ifelse(substr(time, 4, 5) == "00", 0, sub("^0+", "", substr(time, 4, 5))),
+    ifelse(substr(time, 1, 2) == "00", 0, sub("^0+", "", substr(time, 1, 2))),
+    ifelse(substr(time, 7, 8) == "00", 0, sub("^0+", "", substr(time, 7, 8)))
+  )) %>%
+  select(-c("time"))
+
+electricity_demand_reference_germany_sh_and_hw <-
+  electricity_demand_reference_germany_sh_and_hw %>%
+  arrange(DATEISO)
+
+
+electricity_demand_cold_germany_sh_and_hw <-
+  regions_electricity_demand_cold %>%
+  select(-c("nuts3_code")) %>%
+  group_by(time) %>%
+  summarise("Electricity demand" = sum(hourly_electricity_demand),
+            .groups = "drop")
+
+electricity_demand_cold_germany_sh_and_hw <-
+  electricity_demand_cold_germany_sh_and_hw %>%
+  mutate(DATEISO = ISOdate(
+    2030,
+    ifelse(substr(time, 4, 5) == "00", 0, sub("^0+", "", substr(time, 4, 5))),
+    ifelse(substr(time, 1, 2) == "00", 0, sub("^0+", "", substr(time, 1, 2))),
+    ifelse(substr(time, 7, 8) == "00", 0, sub("^0+", "", substr(time, 7, 8)))
+  )) %>%
+  select(-c("time"))
+
+electricity_demand_cold_germany_sh_and_hw <-
+  electricity_demand_cold_germany_sh_and_hw %>%
+  arrange(DATEISO)
+
+
+electricity_demand_hot_germany_sh_and_hw <-
+  regions_electricity_demand_hot %>%
+  select(-c("nuts3_code")) %>%
+  group_by(time) %>%
+  summarise("Electricity demand" = sum(hourly_electricity_demand),
+            .groups = "drop")
+
+electricity_demand_hot_germany_sh_and_hw <-
+  electricity_demand_hot_germany_sh_and_hw %>%
+  mutate(DATEISO = ISOdate(
+    2030,
+    ifelse(substr(time, 4, 5) == "00", 0, sub("^0+", "", substr(time, 4, 5))),
+    ifelse(substr(time, 1, 2) == "00", 0, sub("^0+", "", substr(time, 1, 2))),
+    ifelse(substr(time, 7, 8) == "00", 0, sub("^0+", "", substr(time, 7, 8)))
+  )) %>%
+  select(-c("time"))
+
+electricity_demand_hot_germany_sh_and_hw <-
+  electricity_demand_hot_germany_sh_and_hw %>%
+  arrange(DATEISO)
+
+
+# Space heat only
+electricity_demand_reference_germany_sh_only <-
+  regions_electricity_demand_space_heat_only_reference %>%
+  select(-c("nuts3_code")) %>%
+  group_by(time) %>%
+  summarise("Electricity demand" = sum(hourly_electricity_demand),
+            .groups = "drop")
+
+electricity_demand_reference_germany_sh_only <-
+  electricity_demand_reference_germany_sh_only %>%
+  mutate(DATEISO = ISOdate(
+    2030,
+    ifelse(substr(time, 4, 5) == "00", 0, sub("^0+", "", substr(time, 4, 5))),
+    ifelse(substr(time, 1, 2) == "00", 0, sub("^0+", "", substr(time, 1, 2))),
+    ifelse(substr(time, 7, 8) == "00", 0, sub("^0+", "", substr(time, 7, 8)))
+  )) %>%
+  select(-c("time"))
+
+electricity_demand_reference_germany_sh_only <-
+  electricity_demand_reference_germany_sh_only %>%
+  arrange(DATEISO)
+
+
+electricity_demand_cold_germany_sh_only <-
+  regions_electricity_demand_space_heat_only_cold %>%
+  select(-c("nuts3_code")) %>%
+  group_by(time) %>%
+  summarise("Electricity demand" = sum(hourly_electricity_demand),
+            .groups = "drop")
+
+electricity_demand_cold_germany_sh_only <-
+  electricity_demand_cold_germany_sh_only %>%
+  mutate(DATEISO = ISOdate(
+    2030,
+    ifelse(substr(time, 4, 5) == "00", 0, sub("^0+", "", substr(time, 4, 5))),
+    ifelse(substr(time, 1, 2) == "00", 0, sub("^0+", "", substr(time, 1, 2))),
+    ifelse(substr(time, 7, 8) == "00", 0, sub("^0+", "", substr(time, 7, 8)))
+  )) %>%
+  select(-c("time"))
+
+electricity_demand_cold_germany_sh_only <-
+  electricity_demand_cold_germany_sh_only %>%
+  arrange(DATEISO)
+
+
+electricity_demand_hot_germany_sh_only <-
+  regions_electricity_demand_space_heat_only_hot %>%
+  select(-c("nuts3_code")) %>%
+  group_by(time) %>%
+  summarise("Electricity demand" = sum(hourly_electricity_demand),
+            .groups = "drop")
+
+electricity_demand_hot_germany_sh_only <-
+  electricity_demand_hot_germany_sh_only %>%
+  mutate(DATEISO = ISOdate(
+    2030,
+    ifelse(substr(time, 4, 5) == "00", 0, sub("^0+", "", substr(time, 4, 5))),
+    ifelse(substr(time, 1, 2) == "00", 0, sub("^0+", "", substr(time, 1, 2))),
+    ifelse(substr(time, 7, 8) == "00", 0, sub("^0+", "", substr(time, 7, 8)))
+  )) %>%
+  select(-c("time"))
+
+electricity_demand_hot_germany_sh_only <-
+  electricity_demand_hot_germany_sh_only %>%
+  arrange(DATEISO)
+
+
+# Total electricity demand over year
+
+# Space heat and hot water
+sum(electricity_demand_reference_germany_sh_and_hw$`Electricity demand`) / 1000000000
+sum(electricity_demand_cold_germany_sh_and_hw$`Electricity demand`) / 1000000000
+sum(electricity_demand_hot_germany_sh_and_hw$`Electricity demand`) / 1000000000
+
+# Space heat only
+sum(electricity_demand_reference_germany_sh_only$`Electricity demand`) / 1000000000
+sum(electricity_demand_cold_germany_sh_only$`Electricity demand`) / 1000000000
+sum(electricity_demand_hot_germany_sh_only$`Electricity demand`) / 1000000000
+
+
+# Max hourly electricity demand
+
+# Space heat and hot water
+max(electricity_demand_reference_germany_sh_and_hw$`Electricity demand`) / 1000000
+max(electricity_demand_cold_germany_sh_and_hw$`Electricity demand`) / 1000000
+max(electricity_demand_hot_germany_sh_and_hw$`Electricity demand`) / 1000000
+
+# Space heat only
+max(electricity_demand_reference_germany_sh_only$`Electricity demand`) / 1000000
+max(electricity_demand_cold_germany_sh_only$`Electricity demand`) / 1000000
+max(electricity_demand_hot_germany_sh_only$`Electricity demand`) / 1000000
+
+
+# Mean hourly electricity demand
+
+# Space heat and hot water
+mean(electricity_demand_reference_germany_sh_and_hw$`Electricity demand`) / 1000000
+mean(electricity_demand_cold_germany_sh_and_hw$`Electricity demand`) / 1000000
+mean(electricity_demand_hot_germany_sh_and_hw$`Electricity demand`) / 1000000
+
+# Space heat only
+mean(electricity_demand_reference_germany_sh_only$`Electricity demand`) / 1000000
+mean(electricity_demand_cold_germany_sh_only$`Electricity demand`) / 1000000
+mean(electricity_demand_hot_germany_sh_only$`Electricity demand`) / 1000000
+
+
+# Write output to csv
+write_csv(
+  electricity_demand_reference_germany_sh_and_hw,
+  "data/output/findings/electricity-demand-germany/electricity_demand_reference_germany_sh_and_hw.csv"
+)
+
+write_csv(
+  electricity_demand_cold_germany_sh_and_hw,
+  "data/output/findings/electricity-demand-germany/electricity_demand_cold_germany_sh_and_hw.csv"
+)
+
+write_csv(
+  electricity_demand_hot_germany_sh_and_hw,
+  "data/output/findings/electricity-demand-germany/electricity_demand_hot_germany_sh_and_hw.csv"
+)
+
+
+write_csv(
+  electricity_demand_reference_germany_sh_only,
+  "data/output/findings/electricity-demand-germany/electricity_demand_reference_germany_sh_only.csv"
+)
+
+write_csv(
+  electricity_demand_cold_germany_sh_only,
+  "data/output/findings/electricity-demand-germany/electricity_demand_cold_germany_sh_only.csv"
+)
+
+write_csv(
+  electricity_demand_hot_germany_sh_only,
+  "data/output/findings/electricity-demand-germany/electricity_demand_hot_germany_sh_only.csv"
+)
--- plots/regionselectricitydemand/ElectricityDemandGermanyPlots.R
+++ plots/regionselectricitydemand/ElectricityDemandGermanyPlots.R
@@ -4,256 +4,117 @@ library(readxl)
 library("dplyr")
 
 # Read the regions electricity demand data
-regions_electricity_demand_reference <-
-  read_csv("data/regionselectricitydemand/regions_electricity_demand_reference.csv")
-
-regions_electricity_demand_cold <-
-  read_csv("data/regionselectricitydemand/regions_electricity_demand_cold.csv")
-
-regions_electricity_demand_hot <-
-  read_csv("data/regionselectricitydemand/regions_electricity_demand_hot.csv")
-
-
-regions_electricity_demand_space_heat_only_reference <-
+electricity_demand_reference_germany_sh_and_hw <-
   read_csv(
-    "data/regionselectricitydemand/regions_electricity_demand_space_heat_only_reference.csv"
+    "data/output/findings/electricity-demand-germany/electricity_demand_reference_germany_sh_and_hw.csv"
   )
 
-regions_electricity_demand_space_heat_only_cold <-
+electricity_demand_cold_germany_sh_and_hw <-
   read_csv(
-    "data/regionselectricitydemand/regions_electricity_demand_space_heat_only_cold.csv"
+    "data/output/findings/electricity-demand-germany/electricity_demand_cold_germany_sh_and_hw.csv"
   )
 
-regions_electricity_demand_space_heat_only_hot <-
+electricity_demand_hot_germany_sh_and_hw <-
   read_csv(
-    "data/regionselectricitydemand/regions_electricity_demand_space_heat_only_hot.csv"
+    "data/output/findings/electricity-demand-germany/electricity_demand_hot_germany_sh_and_hw.csv"
+  )
+
+
+electricity_demand_reference_germany_sh_only <-
+  read_csv(
+    "data/output/findings/electricity-demand-germany/electricity_demand_reference_germany_sh_only.csv"
+  )
+
+electricity_demand_cold_germany_sh_only <-
+  read_csv(
+    "data/output/findings/electricity-demand-germany/electricity_demand_cold_germany_sh_only.csv"
+  )
+
+electricity_demand_hot_germany_sh_only <-
+  read_csv(
+    "data/output/findings/electricity-demand-germany/electricity_demand_hot_germany_sh_only.csv"
   )
 
 
 # Plot the combined electricity demand fo Germany
 
 # For space heating and hot water
-electricity_demand_reference_germany_sh_and_hw <-
-  regions_electricity_demand_reference %>%
-  select(-c("nuts3_code")) %>%
-  group_by(time) %>%
-  summarise("Electricity demand" = sum(hourly_electricity_demand),
-            .groups = "drop")
 
 electricity_demand_reference_germany_sh_and_hw <-
   electricity_demand_reference_germany_sh_and_hw %>%
-  mutate(DATEISO = ISOdate(
-    2017,
-    ifelse(substr(time, 4, 5) == "00", 0, sub("^0+", "", substr(time, 4, 5))),
-    ifelse(substr(time, 1, 2) == "00", 0, sub("^0+", "", substr(time, 1, 2))),
-    ifelse(substr(time, 7, 8) == "00", 0, sub("^0+", "", substr(time, 7, 8)))
-  )) %>%
-  select(-c("time"))
+  mutate("Temperature series" = 2017)
 
-electricity_demand_reference_germany_sh_and_hw <-
+electricity_demand_cold_germany_sh_and_hw <-
+  electricity_demand_cold_germany_sh_and_hw %>%
+  mutate("Temperature series" = 2010)
+
+electricity_demand_hot_germany_sh_and_hw <-
+  electricity_demand_hot_germany_sh_and_hw %>%
+  mutate("Temperature series" = 2022)
+
+
+electricity_demand_germany_sh_and_hw <-
   electricity_demand_reference_germany_sh_and_hw %>%
-  arrange(DATEISO)
+  rbind(electricity_demand_cold_germany_sh_and_hw) %>%
+  rbind(electricity_demand_hot_germany_sh_and_hw)
 
-electricity_demand_germany_reference_sh_and_hw_plot <-
-  ggplot(data = electricity_demand_reference_germany_sh_and_hw, aes(DATEISO, `Electricity demand` / 1000000)) +
-  geom_line(lwd = 0.5) + ylab("Electricity demand in GWh") + xlab("Date") + coord_cartesian(ylim = c(0, 55))
-
-electricity_demand_germany_reference_sh_and_hw_plot
-
-sum(electricity_demand_reference_germany_sh_and_hw$`Electricity demand`)
-
-
-electricity_demand_cold_germany_sh_and_hw <-
-  regions_electricity_demand_cold %>%
-  select(-c("nuts3_code")) %>%
-  group_by(time) %>%
-  summarise("Electricity demand" = sum(hourly_electricity_demand),
-            .groups = "drop")
-
-electricity_demand_cold_germany_sh_and_hw <-
-  electricity_demand_cold_germany_sh_and_hw %>%
-  mutate(DATEISO = ISOdate(
-    2010,
-    ifelse(substr(time, 4, 5) == "00", 0, sub("^0+", "", substr(time, 4, 5))),
-    ifelse(substr(time, 1, 2) == "00", 0, sub("^0+", "", substr(time, 1, 2))),
-    ifelse(substr(time, 7, 8) == "00", 0, sub("^0+", "", substr(time, 7, 8)))
-  )) %>%
-  select(-c("time"))
-
-electricity_demand_cold_germany_sh_and_hw <-
-  electricity_demand_cold_germany_sh_and_hw %>%
-  arrange(DATEISO)
-
-electricity_demand_germany_cold_sh_and_hw_plot <-
-  ggplot(data = electricity_demand_cold_germany_sh_and_hw, aes(DATEISO, `Electricity demand` / 1000000)) +
-  geom_line(lwd = 0.5) + ylab("Electricity demand in GWh") + xlab("Date") + coord_cartesian(ylim = c(0, 55))
-
-electricity_demand_germany_cold_sh_and_hw_plot
-
-sum(electricity_demand_cold_germany_sh_and_hw$`Electricity demand`)
+electricity_demand_germany_sh_and_hw_plot <-   ggplot(data = electricity_demand_germany_sh_and_hw,
+                                                      aes(DATEISO,
+                                                          `Electricity demand` / 1000000)) +
+  geom_line(lwd = 0.5) +
+  facet_wrap(~ `Temperature series`, ncol = 1) +
+  coord_cartesian(ylim = c(0, 55)) +
+  labs(x = "Date",
+       y = "Electricity demand in GWh")
 
 
-electricity_demand_hot_germany_sh_and_hw <-
-  regions_electricity_demand_hot %>%
-  select(-c("nuts3_code")) %>%
-  group_by(time) %>%
-  summarise("Electricity demand" = sum(hourly_electricity_demand),
-            .groups = "drop")
-
-electricity_demand_hot_germany_sh_and_hw <-
-  electricity_demand_hot_germany_sh_and_hw %>%
-  mutate(DATEISO = ISOdate(
-    2022,
-    ifelse(substr(time, 4, 5) == "00", 0, sub("^0+", "", substr(time, 4, 5))),
-    ifelse(substr(time, 1, 2) == "00", 0, sub("^0+", "", substr(time, 1, 2))),
-    ifelse(substr(time, 7, 8) == "00", 0, sub("^0+", "", substr(time, 7, 8)))
-  )) %>%
-  select(-c("time"))
-
-electricity_demand_hot_germany_sh_and_hw <-
-  electricity_demand_hot_germany_sh_and_hw %>%
-  arrange(DATEISO)
-
-electricity_demand_germany_hot_sh_and_hw_plot <-
-  ggplot(data = electricity_demand_hot_germany_sh_and_hw, aes(DATEISO, `Electricity demand` / 1000000)) +
-  geom_line(lwd = 0.5) + ylab("Electricity demand in GWh") + xlab("Date") + coord_cartesian(ylim = c(0, 55))
-
-electricity_demand_germany_hot_sh_and_hw_plot
-
-sum(electricity_demand_hot_germany_sh_and_hw$`Electricity demand`)
+electricity_demand_germany_sh_and_hw_plot
 
 
 # For space heating only
 electricity_demand_reference_germany_sh_only <-
-  regions_electricity_demand_space_heat_only_reference %>%
-  select(-c("nuts3_code")) %>%
-  group_by(time) %>%
-  summarise("Electricity demand" = sum(hourly_electricity_demand),
-            .groups = "drop")
-
-electricity_demand_reference_germany_sh_only <-
   electricity_demand_reference_germany_sh_only %>%
-  mutate(DATEISO = ISOdate(
-    2017,
-    ifelse(substr(time, 4, 5) == "00", 0, sub("^0+", "", substr(time, 4, 5))),
-    ifelse(substr(time, 1, 2) == "00", 0, sub("^0+", "", substr(time, 1, 2))),
-    ifelse(substr(time, 7, 8) == "00", 0, sub("^0+", "", substr(time, 7, 8)))
-  )) %>%
-  select(-c("time"))
-
-electricity_demand_reference_germany_sh_only <-
-  electricity_demand_reference_germany_sh_only %>%
-  arrange(DATEISO)
-
-electricity_demand_germany_reference_sh_only_plot <-
-  ggplot(data = electricity_demand_reference_germany_sh_only, aes(DATEISO, `Electricity demand` / 1000000)) +
-  geom_line(lwd = 0.5) + ylab("Electricity demand in GWh") + xlab("Date") + coord_cartesian(ylim = c(0, 55))
-
-electricity_demand_germany_reference_sh_only_plot
-
-sum(electricity_demand_reference_germany_sh_only$`Electricity demand`)
-
-
-electricity_demand_cold_germany_sh_only <-
-  regions_electricity_demand_space_heat_only_cold %>%
-  select(-c("nuts3_code")) %>%
-  group_by(time) %>%
-  summarise("Electricity demand" = sum(hourly_electricity_demand),
-            .groups = "drop")
+  mutate("Temperature series" = 2017)
 
 electricity_demand_cold_germany_sh_only <-
   electricity_demand_cold_germany_sh_only %>%
-  mutate(DATEISO = ISOdate(
-    2010,
-    ifelse(substr(time, 4, 5) == "00", 0, sub("^0+", "", substr(time, 4, 5))),
-    ifelse(substr(time, 1, 2) == "00", 0, sub("^0+", "", substr(time, 1, 2))),
-    ifelse(substr(time, 7, 8) == "00", 0, sub("^0+", "", substr(time, 7, 8)))
-  )) %>%
-  select(-c("time"))
-
-electricity_demand_cold_germany_sh_only <-
-  electricity_demand_cold_germany_sh_only %>%
-  arrange(DATEISO)
-
-electricity_demand_germany_cold_sh_only_plot <-
-  ggplot(data = electricity_demand_cold_germany_sh_only, aes(DATEISO, `Electricity demand` / 1000000)) +
-  geom_line(lwd = 0.5) + ylab("Electricity demand in GWh") + xlab("Date") + coord_cartesian(ylim = c(0, 55))
-
-electricity_demand_germany_cold_sh_only_plot
-
-sum(electricity_demand_cold_germany_sh_only$`Electricity demand`)
-
-
-electricity_demand_hot_germany_sh_only <-
-  regions_electricity_demand_space_heat_only_hot %>%
-  select(-c("nuts3_code")) %>%
-  group_by(time) %>%
-  summarise("Electricity demand" = sum(hourly_electricity_demand),
-            .groups = "drop")
+  mutate("Temperature series" = 2010)
 
 electricity_demand_hot_germany_sh_only <-
   electricity_demand_hot_germany_sh_only %>%
-  mutate(DATEISO = ISOdate(
-    2022,
-    ifelse(substr(time, 4, 5) == "00", 0, sub("^0+", "", substr(time, 4, 5))),
-    ifelse(substr(time, 1, 2) == "00", 0, sub("^0+", "", substr(time, 1, 2))),
-    ifelse(substr(time, 7, 8) == "00", 0, sub("^0+", "", substr(time, 7, 8)))
-  )) %>%
-  select(-c("time"))
+  mutate("Temperature series" = 2022)
 
-electricity_demand_hot_germany_sh_only <-
-  electricity_demand_hot_germany_sh_only %>%
-  arrange(DATEISO)
 
-electricity_demand_germany_hot_sh_only_plot <-
-  ggplot(data = electricity_demand_hot_germany_sh_only, aes(DATEISO, `Electricity demand` / 1000000)) +
-  geom_line(lwd = 0.5) + ylab("Electricity demand in GWh") + xlab("Date") + coord_cartesian(ylim = c(0, 55))
+electricity_demand_germany_sh_only <-
+  electricity_demand_reference_germany_sh_only %>%
+  rbind(electricity_demand_cold_germany_sh_only) %>%
+  rbind(electricity_demand_hot_germany_sh_only)
 
-electricity_demand_germany_hot_sh_only_plot
 
-sum(electricity_demand_hot_germany_sh_only$`Electricity demand`)
+electricity_demand_germany_sh_only_plot <-   ggplot(data = electricity_demand_germany_sh_only,
+                                                      aes(DATEISO,
+                                                          `Electricity demand` / 1000000)) +
+  geom_line(lwd = 0.5) +
+  facet_wrap(~ `Temperature series`, ncol = 1) +
+  coord_cartesian(ylim = c(0, 55)) +
+  labs(x = "Date",
+       y = "Electricity demand in GWh")
+
+
+electricity_demand_germany_sh_only_plot
 
 
 # Save the plots
 ggsave(
-  "plots/output/regionselectricitydemand/electricitydemandgermanyovertime/electricity_demand_germany_reference_sh_and_hw_plot.png",
-  electricity_demand_germany_reference_sh_and_hw_plot,
-  width = 25,
+  "plots/output/regionselectricitydemand/electricitydemandgermanyovertime/electricity_demand_germany_sh_and_hw_plot.pdf",
+  electricity_demand_germany_sh_and_hw_plot,
+  width = 30,
   units = "cm"
 )
 
 ggsave(
-  "plots/output/regionselectricitydemand/electricitydemandgermanyovertime/electricity_demand_germany_cold_sh_and_hw_plot.png",
-  electricity_demand_germany_cold_sh_and_hw_plot,
-  width = 25,
-  units = "cm"
-)
-
-ggsave(
-  "plots/output/regionselectricitydemand/electricitydemandgermanyovertime/electricity_demand_germany_hot_sh_and_hw_plot.png",
-  electricity_demand_germany_hot_sh_and_hw_plot,
-  width = 25,
-  units = "cm"
-)
-
-
-ggsave(
-  "plots/output/regionselectricitydemand/electricitydemandgermanyovertime/electricity_demand_germany_reference_sh_only_plot.png",
-  electricity_demand_germany_reference_sh_only_plot,
-  width = 25,
-  units = "cm"
-)
-
-ggsave(
-  "plots/output/regionselectricitydemand/electricitydemandgermanyovertime/electricity_demand_germany_cold_sh_only_plot.png",
-  electricity_demand_germany_cold_sh_only_plot,
-  width = 25,
-  units = "cm"
-)
-
-ggsave(
-  "plots/output/regionselectricitydemand/electricitydemandgermanyovertime/electricity_demand_germany_hot_sh_only_plot.png",
-  electricity_demand_germany_hot_sh_only_plot,
-  width = 25,
+  "plots/output/regionselectricitydemand/electricitydemandgermanyovertime/electricity_demand_germany_sh_only_plot.pdf",
+  electricity_demand_germany_sh_only_plot,
+  width = 30,
   units = "cm"
 )
