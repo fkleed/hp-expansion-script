fc6a418013db08e62db015ee83315447090a0965
Ferenc Kleedörfer <ferenc.kleedoerfer@outlook.de>
2023-10-14 17:54:24 +0200

    Updated plot for annual and maximum hourly hp electricity demands on NUTS 3 regions

---
+++ plots/regionselectricitydemand/AnnualElectricityDemandNUTS3Highest10Plots.R
@@ -0,0 +1,225 @@
+# Load required packages
+library(tidyverse)
+library("dplyr")
+library(readxl)
+
+# Read the data
+nuts3regioninfo <-
+  read_excel("data/buildingstructure/nuts3regioninfo.xlsx") %>%
+  mutate(
+    NUTS3Type = fct_recode(
+      NUTS3Type,
+      "District" = "Kreis",
+      "Urban district" = "Kreisfreie Stadt",
+      "Urban district" = "Stadtkreis",
+      "Rural district" = "Landkreis",
+      "Regional association" = "Regionalverband"
+    ),
+    NUTS3Name = paste(gsub(",.*", "", NUTS3Name), NUTS3Type, sep = ", ")
+    ) %>%
+  select(c("NUTS3Code", "NUTS3Name"))
+
+
+regions_electricity_demand_reference_iso <-
+  read_csv(
+    "data/output/findings/electricity-demand-nuts3/regions_electricity_demand_reference_iso.csv"
+  )
+
+regions_electricity_demand_cold_iso <-
+  read_csv(
+    "data/output/findings/electricity-demand-nuts3/regions_electricity_demand_cold_iso.csv"
+  )
+
+regions_electricity_demand_hot_iso <-
+  read_csv(
+    "data/output/findings/electricity-demand-nuts3/regions_electricity_demand_hot_iso.csv"
+  )
+
+
+regions_electricity_demand_space_heat_only_reference_iso <-
+  read_csv(
+    "data/output/findings/electricity-demand-nuts3/regions_electricity_demand_space_heat_only_reference_iso.csv"
+  )
+
+regions_electricity_demand_space_heat_only_cold_iso <-
+  read_csv(
+    "data/output/findings/electricity-demand-nuts3/regions_electricity_demand_space_heat_only_cold_iso.csv"
+  )
+
+regions_electricity_demand_space_heat_only_hot_iso <-
+  read_csv(
+    "data/output/findings/electricity-demand-nuts3/regions_electricity_demand_space_heat_only_hot_iso.csv"
+  )
+
+# Calculate the 10 NUTS 3 regions with the highest annual heat pump electricity demand for space heating and hot water
+
+# Reference year
+regions_electricity_demand_reference <-
+  regions_electricity_demand_reference_iso %>%
+  select(-c("date_iso")) %>%
+  group_by(nuts3_code) %>%
+  summarise("Electricity demand" = sum(hourly_electricity_demand),
+            .groups = "drop")
+
+highest_regions_electricity_demand_sh_and_hw_reference <-
+  regions_electricity_demand_reference %>%
+  slice_max(`Electricity demand`, n = 10) %>%
+  left_join(nuts3regioninfo, by = c("nuts3_code" = "NUTS3Code"))
+
+# Cold year
+regions_electricity_demand_cold <-
+  regions_electricity_demand_cold_iso %>%
+  select(-c("date_iso")) %>%
+  group_by(nuts3_code) %>%
+  summarise("Electricity demand" = sum(hourly_electricity_demand),
+            .groups = "drop")
+
+highest_regions_electricity_demand_sh_and_hw_cold <-
+  regions_electricity_demand_cold %>%
+  slice_max(`Electricity demand`, n = 10) %>%
+  left_join(nuts3regioninfo, by = c("nuts3_code" = "NUTS3Code"))
+
+# Hot year
+regions_electricity_demand_hot <-
+  regions_electricity_demand_hot_iso %>%
+  select(-c("date_iso")) %>%
+  group_by(nuts3_code) %>%
+  summarise("Electricity demand" = sum(hourly_electricity_demand),
+            .groups = "drop")
+
+highest_regions_electricity_demand_sh_and_hw_hot <-
+  regions_electricity_demand_hot %>%
+  slice_max(`Electricity demand`, n = 10) %>%
+  left_join(nuts3regioninfo, by = c("nuts3_code" = "NUTS3Code"))
+
+
+# Calculate the 10 NUTS 3 regions with the highest annual heat pump electricity demand for space heating only
+
+# Reference year
+regions_electricity_demand_space_heat_only_reference <-
+  regions_electricity_demand_space_heat_only_reference_iso %>%
+  select(-c("date_iso")) %>%
+  group_by(nuts3_code) %>%
+  summarise("Electricity demand" = sum(hourly_electricity_demand),
+            .groups = "drop")
+
+highest_regions_electricity_demand_sh_only_reference <-
+  regions_electricity_demand_space_heat_only_reference %>%
+  slice_max(`Electricity demand`, n = 10) %>%
+  left_join(nuts3regioninfo, by = c("nuts3_code" = "NUTS3Code"))
+
+# Cold year
+regions_electricity_demand_space_heat_only_cold <-
+  regions_electricity_demand_space_heat_only_cold_iso %>%
+  select(-c("date_iso")) %>%
+  group_by(nuts3_code) %>%
+  summarise("Electricity demand" = sum(hourly_electricity_demand),
+            .groups = "drop")
+
+highest_regions_electricity_demand_sh_only_cold <-
+  regions_electricity_demand_space_heat_only_cold %>%
+  slice_max(`Electricity demand`, n = 10) %>%
+  left_join(nuts3regioninfo, by = c("nuts3_code" = "NUTS3Code"))
+
+# Hot year
+regions_electricity_demand_space_heat_only_hot <-
+  regions_electricity_demand_space_heat_only_hot_iso %>%
+  select(-c("date_iso")) %>%
+  group_by(nuts3_code) %>%
+  summarise("Electricity demand" = sum(hourly_electricity_demand),
+            .groups = "drop")
+
+highest_regions_electricity_demand_sh_only_hot <-
+  regions_electricity_demand_space_heat_only_hot %>%
+  slice_max(`Electricity demand`, n = 10) %>%
+  left_join(nuts3regioninfo, by = c("nuts3_code" = "NUTS3Code"))
+
+
+# Combine data and plot the graphs
+
+# Space heating and hot water
+highest_regions_electricity_demand_sh_and_hw_reference <-
+  highest_regions_electricity_demand_sh_and_hw_reference %>%
+  mutate("Year" = 2017)
+
+highest_regions_electricity_demand_sh_and_hw_cold <-
+  highest_regions_electricity_demand_sh_and_hw_cold %>%
+  mutate("Year" = 2010)
+
+highest_regions_electricity_demand_sh_and_hw_hot <-
+  highest_regions_electricity_demand_sh_and_hw_hot %>%
+  mutate("Year" = 2022)
+
+highest_regions_electricity_demand_sh_and_hw <-
+  highest_regions_electricity_demand_sh_and_hw_reference %>%
+  rbind(highest_regions_electricity_demand_sh_and_hw_cold) %>%
+  rbind(highest_regions_electricity_demand_sh_and_hw_hot)
+
+highest_regions_annual_electricity_demand_sh_and_hw_plot <-
+  ggplot(
+    data = highest_regions_electricity_demand_sh_and_hw,
+    aes(
+      x = `Electricity demand` / 1000000,
+      y = reorder(NUTS3Name,-`Electricity demand`)
+    )
+  ) +
+  geom_bar(stat = "identity") +
+  coord_cartesian(xlim = c(0, 1250)) +
+  facet_wrap(~Year, ncol = 1) +
+  labs(x = "Electricity demand in GWh",
+       y = "NUTS 3 Name")
+
+
+highest_regions_annual_electricity_demand_sh_and_hw_plot
+
+
+# Space heating only
+highest_regions_electricity_demand_sh_only_reference <-
+  highest_regions_electricity_demand_sh_only_reference %>%
+  mutate("Year" = 2017)
+
+highest_regions_electricity_demand_sh_only_cold <-
+  highest_regions_electricity_demand_sh_only_cold %>%
+  mutate("Year" = 2010)
+
+highest_regions_electricity_demand_sh_only_hot <-
+  highest_regions_electricity_demand_sh_only_hot %>%
+  mutate("Year" = 2022)
+
+highest_regions_electricity_demand_sh_only <-
+  highest_regions_electricity_demand_sh_only_reference %>%
+  rbind(highest_regions_electricity_demand_sh_only_cold) %>%
+  rbind(highest_regions_electricity_demand_sh_only_hot)
+
+highest_regions_annual_electricity_demand_sh_only_plot <-
+  ggplot(
+    data = highest_regions_electricity_demand_sh_only,
+    aes(
+      x = `Electricity demand` / 1000000,
+      y = reorder(NUTS3Name,-`Electricity demand`)
+    )
+  ) +
+  geom_bar(stat = "identity") +
+  coord_cartesian(xlim = c(0, 1250)) +
+  facet_wrap(~Year, ncol = 1) +
+  labs(x = "Electricity demand in GWh",
+       y = "NUTS 3 Name")
+
+
+highest_regions_annual_electricity_demand_sh_only_plot
+
+
+# Save the plots
+ggsave(
+  "plots/output/regionselectricitydemand/annualelectricitydemandnuts3highest10/highest_regions_annual_electricity_demand_sh_and_hw_plot.png",
+  highest_regions_annual_electricity_demand_sh_and_hw_plot,
+  width = 30,
+  units = "cm"
+)
+
+ggsave(
+  "plots/output/regionselectricitydemand/annualelectricitydemandnuts3highest10/highest_regions_annual_electricity_demand_sh_only_plot.png",
+  highest_regions_annual_electricity_demand_sh_only_plot,
+  width = 30,
+  units = "cm"
+)
